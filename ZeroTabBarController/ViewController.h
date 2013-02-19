f3dfd154d6675706c417ebc207025eb1ba4a2250
Rakesh <rakesh@mysgec212271d.local>
2013-02-19 14:09:53 +0530

    Initial Commit

---
+++ ZeroTabBarController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ZeroTabBarController
+//
+//  Created by Rakesh on 19/02/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
